1b12a051897f75e679f07a52f5a63b93bd35bbc1
saadansaryy <ansarisaad990@gmail.com>
2022-08-17 12:57:35 +0530

    Updates

---
+++ precache-manifest.fc639de02d30d38fc929d7420fef6bfa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a6865dcbfa9f8f32eec2",
+    "url": "/text-analyzer-react/static/css/main.d73fb73a.chunk.css"
+  },
+  {
+    "revision": "a6865dcbfa9f8f32eec2",
+    "url": "/text-analyzer-react/static/js/main.a6865dcb.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/text-analyzer-react/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/text-analyzer-react/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "3f4b5792fd9726567641",
+    "url": "/text-analyzer-react/static/js/runtime~main.3f4b5792.js"
+  },
+  {
+    "revision": "79653b6c7677307a86a50f3a20016b96",
+    "url": "/text-analyzer-react/index.html"
+  }
+];
